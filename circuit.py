5febf34509b50c9c479f82af2f054701f2aaf107
siyuchen95 <earthshinecsy@gmail.com>
2023-02-17 22:36:09 +0000

    Update and rename Circuit.py to circuit.py

--- circuit.py
+++ circuit.py
@@ -14,44 +14,60 @@ class Circuit:
 
 
 class A0B0Circuit(Circuit):
+    code = '00'
+    name = 'A0B0'
+    
     def __init__(self):
         Circuit.__init__(self)
-        self.name = 'A0B0'
 
         self.activate_bell_state()
         self.circuit.z(0)
         self.circuit.h(1)
+        
+        self.measure()
 
 
 class A0B1Circuit(Circuit):
+    code = '01'
+    name = 'A0B1'    
+    
     def __init__(self):
         Circuit.__init__(self)
-        self.name = 'A0B1'
 
         self.activate_bell_state()
         self.circuit.z(0)
         self.circuit.z(1)
         self.circuit.x(1)
         self.circuit.h(1)
+        
+        self.measure()
 
 
 class A1B0Circuit(Circuit):
+    code = '10'
+    name = 'A1B0'
+
     def __init__(self):
         Circuit.__init__(self)
-        self.name = 'A1B0'
 
         self.activate_bell_state()
         self.circuit.x(0)
         self.circuit.h(1)
+        
+        self.measure()
 
 
 class A1B1Circuit(Circuit):
+    code = '11'
+    name = 'A1B1'
+
     def __init__(self):
         Circuit.__init__(self)
-        self.name = 'A1B1'
-
+        
         self.activate_bell_state()
         self.circuit.x(0)
         self.circuit.z(1)
         self.circuit.x(1)
         self.circuit.h(1)
+        
+        self.measure()
